dd679d3d910bd0a8af6a68131655c5034cf2921d
KiranMotukuru <kiranmotukuru@gmail.com>
2025-10-18 12:56:32 +0530

    updated feign client

--- UserService/src/main/java/com/tcs/ProductClient.java
+++ UserService/src/main/java/com/tcs/ProductClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "product-service", url = "http://localhost:8081/products")
+@FeignClient(name = "product-service", url = "http://product-service:8081/products")
 public interface ProductClient {
     @GetMapping("/{id}")
     String getProductById(@PathVariable("id") String id);
